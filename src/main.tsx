85a36894aad9e2f8765332269511ee8d54501045
personal <hasinmahmood01@gmail.com>
2023-07-25 23:33:07 +0100

    fix:correct vercel analytics to work with react

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
 import "./index.css";
-import { inject } from "@vercel/analytics";
-
-inject();
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <Analytics mode={"production"} />;
     <App />
   </React.StrictMode>
 );
