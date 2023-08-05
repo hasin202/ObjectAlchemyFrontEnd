1aceb707c4b6725737aa24a0ed7771fb27eb6f06
personal <hasinmahmood01@gmail.com>
2023-08-05 20:26:22 +0100

    feat: update react parser being used

--- src/components/page.tsx
+++ src/components/page.tsx
@@ -2,10 +2,10 @@ import React, { useEffect, useState } from "react";
 import RequestForm from "./requestForm";
 import IResponse from "../types/response";
 import Error from "./error";
-import ReactJson from "react-json-view-c2";
 import Loader from "./loader";
 import DropDown from "./guideDropDown";
 import "../index.css";
+import { JsonViewer, defineDataType } from "@textea/json-viewer";
 
 const Page: React.FC = () => {
   const [response, setResponse] = useState<IResponse>({
@@ -24,6 +24,17 @@ const Page: React.FC = () => {
 
   const [loading, setLoading] = useState<boolean>(false);
 
+  const imageDataType = defineDataType({
+    is: (value) =>
+      typeof value === "string" &&
+      value.startsWith("https://oaidalleapiprodscus.blob"),
+    Component: (props: { value: string }) => (
+      <a href={props.value} target="_blank">
+        {props.value}
+      </a>
+    ),
+  });
+
   return (
     <div className="flex flex-col gap-8 w-full min-[600px]:w-1/2">
       <div className="flex w-full justify-between">
@@ -39,7 +50,10 @@ const Page: React.FC = () => {
       {loading && <Loader />}
       {!loading && (
         <div className="sm:w-96 md:w-full">
-          {isDataAvailable && <ReactJson src={response.data} />}
+          {/* {isDataAvailable && <ReactJson src={response.data} />} */}
+          {isDataAvailable && (
+            <JsonViewer value={response.data} valueTypes={[imageDataType]} />
+          )}
         </div>
       )}
     </div>
