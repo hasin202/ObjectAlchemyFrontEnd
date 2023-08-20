fa6ec66eec4efc8192f32e1d3661269a1a3776b9
personal <hasinmahmood01@gmail.com>
2023-08-20 16:54:45 +0100

    fix: remove unnecessary typing

--- src/styles.ts
+++ src/styles.ts
@@ -1,5 +1,5 @@
-export const input:string =
+export const input =
   "bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500";
 
-export const btn:string =
+export const btn =
   "shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded disabled:bg-purple-300";
